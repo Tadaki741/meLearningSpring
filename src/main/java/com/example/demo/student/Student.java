6a48814043447e38279d614af8b5f71b22028268
Tadaki741 <94947002+Tadaki741@users.noreply.github.com>
2022-03-14 16:22:34 +0700

    fix @Enabled to @Entity, why would it automatically changed to @Enabled ? i dont know

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,9 +1,28 @@
 package com.example.demo.student;
 
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+/*
+@Entity is for Hibernate
+@Table is the use inside the database
+ */
+
+@Entity
+@Table
 public class Student {
 
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private long id;
     private String name;
     private Integer age;
